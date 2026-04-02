4e950c769e9ce85f3bd150aaff4dc30a3519f343
gabriel <gabri02gomes@hotmail.com>
2026-04-02 12:22:28 -0300

    feature: models e migrations add

---
+++ src/models/Livro.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database/sequelize');
+
+const Livro = sequelize.define('Livro', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  titulo: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+  },
+  autor: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+  },
+  disponivel: {
+    type: DataTypes.BOOLEAN,
+    allowNull: true
+  }
+}, {
+  tableName: 'livros',
+  timestamps: true,
+  underscored: false,
+});
+
+module.exports = Livro;
