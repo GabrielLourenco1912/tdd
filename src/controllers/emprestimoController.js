9f4305cb07825453e5da94ae3a4cd6d18a745a8e
gabriel <gabri02gomes@hotmail.com>
2026-04-03 12:27:08 -0300

    feature: add emprestimoController

---
+++ src/controllers/emprestimoController.js
@@ -0,0 +1,40 @@
+const { criarEmprestimo, listarEmprestimo, pegarPorId, deletarEmprestimo, atualizarEmprestimo } = require('../services/emprestimos');
+
+const criar = async (req, res) => {
+    const { usuario_id, livro_id, data_devolucao_prevista } = req.body;
+
+    if (!usuario_id || !livro_id || !data_devolucao_prevista) return res.status(400)
+        .json({ erro: 'livro, usuario e data de devolução são obrigatórios'})
+
+    const emprestimo = await criarEmprestimo(usuario_id, livro_id, data_devolucao_prevista);
+    res.status(201).json(emprestimo);
+}
+
+const listar = async (req, res) => {
+    const emprestimos = await listarEmprestimo();
+    res.status(200).json(emprestimos);
+}
+
+const buscarPorId = async (req, res) => {
+    const { id } = req.params;
+    if (!id) return res.status(400).json({ erro: 'id é obrigatório' });
+    const emprestimos = await pegarPorId(id);
+    res.status(200).json(emprestimos);
+}
+
+const atualizar = async (req, res) => {
+    const { id } = req.params;
+    const { usuario_id, livro_id, data_devolucao_prevista } = req.body;
+    if (!id) return res.status(400).json({ erro: 'id é obrigatório' });
+    const emprestimos = await atualizarEmprestimo(usuario_id, livro_id, data_devolucao_prevista, id);
+    res.status(201).json(emprestimos);
+}
+
+const deletar = async (req, res) => {
+    const { id } = req.params;
+    if (!id) return res.status(400).json({ erro: 'id é obrigatório' });
+    await deletarEmprestimo(id);
+    res.status(204).send();
+}
+
+module.exports = { criar, listar, deletar, buscarPorId, atualizar };
