860a5517f9ddcf92e4e8320177800c0dc681e2c2
gabriel <gabri02gomes@hotmail.com>
2026-04-14 13:02:19 -0300

    feat: add routes multas

---
+++ src/routes/multaRoutes.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+const { criar, listar, deletar, buscarPorId, atualizar, listarPorUsuario } = require('../controllers/multaControllers');
+
+const router = Router();
+
+router.post("/", criar);
+router.get("/", listar);
+router.get("/usuarios/:usuarioId", listarPorUsuario );
+router.get("/:id", buscarPorId);
+router.put("/:id", atualizar);
+router.delete('/:id', deletar);
+
+module.exports = router;
